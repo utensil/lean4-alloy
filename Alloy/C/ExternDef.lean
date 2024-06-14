867e98c8605c6bad2c066f43ef9c7cb2c66c9034
utensil <utensilcandel@gmail.com>
2024-06-14 16:14:05 +0800

    Pass `lake build`

--- Alloy/C/ExternDef.lean
+++ Alloy/C/ExternDef.lean
@@ -16,9 +16,7 @@ whose definition is provided here. That is:
 alloy c extern "alloy_foo" def foo (x : UInt32) : UInt32 := {...}
 ```
 
-
 is essentially equivalent to
-
 
 ```
 @[extern "alloy_foo"] opaque foo (x : UInt32) : UInt32
@@ -38,57 +36,48 @@ elab_rules : command
   let bvs ← liftMacroM <| bs.concatMapM matchBinder
   elabExternImpl exTk sym? ⟨id.raw[0]⟩ bvs ty (packBody stmts)
 
-
+-- declaring this syntax si we can use it in the line marked "leanCppExport" below
 scoped syntax (name := leanCppExport) "extern \"C\"" : cDeclSpec
 
-scoped elab (name := externCppDecl) doc?:(docComment)?
-"alloy " &"cpp " ex:&"extern " sym?:(str)? attrs?:(Term.attributes)?
-"def " id:declId bx:binders " : " type:term " := " body:cStmt : command => do
+-- this is a copy of `elabExternImpl` with modifications marked with "mod begin/end"
+def elabExternImplCpp (exTk : Syntax) (sym? : Option StrLit) (id : Ident) (bvs : Array BinderSyntaxView)
+(type : Syntax) (body : CompStmt) : CommandElabM Unit := do
+  let name ← liftCoreM <| realizeGlobalConstNoOverloadWithInfo id
+  let (cId, extSym) :=
+    match sym? with
+    | some sym =>
+      (mkIdentFrom sym (.mkSimple sym.getString), sym.getString)
+    | none =>
+      let extSym := "_alloy_c_" ++ name.mangle
+      (mkIdentFrom id (.mkSimple extSym), extSym)
+  withRef id <| setExtern name extSym
+  let env ← getEnv
+  let some info := env.find? name
+    | throwErrorAt id "failed to find Lean definition"
+  let some decl := IR.findEnvDecl env name
+    | throwErrorAt id "failed to find Lean IR definition"
+  let ty ← liftMacroM <| MonadRef.withRef type <| expandIrResultTypeToC false decl.resultType
+  let params ← liftTermElabM <| mkParams info.type bvs decl.params
+
+  -- mod begin
+  -- leanCppExport: prefix the function with `extern "C"`
+  let fn ← MonadRef.withRef Syntax.missing <| `(function|
+    extern "C" LEAN_EXPORT%$exTk $ty:cTypeSpec $cId:ident($params:params) $body:compStmt
+  )
+  -- mod end
+
+  let cmd ← `(alloy c section $fn:function end)
+  withMacroExpansion (← getRef) cmd <| elabCommand cmd
+
+scoped syntax (name := externCppDecl)
+(docComment)? (Term.attributes)? "alloy " &"cpp " &"extern " (str)?
+(visibility)? «unsafe»? "def " declId binders " : " term " := " stmtSeq
+: command
 
 elab_rules : command
-| `(externDecl| $[$doc?]? $[$attrs?]? alloy c extern%$exTk $[$sym?]?
+| `(externCppDecl| $[$doc?]? $[$attrs?]? alloy cpp extern%$exTk $[$sym?]?
   $[$vis?]? $[unsafe%$uTk?]? def $id $bs* : $ty := $stmts*) => do
   let cmd ← `($[$doc?]? $[$attrs?]? $[$vis?]? noncomputable $[unsafe%$uTk?]? opaque $id $[$bs]* : $ty)
   withMacroExpansion (← getRef) cmd <| elabCommand cmd
   let bvs ← liftMacroM <| bs.concatMapM matchBinder
-  let id := mkIdentFrom symLit (Name.mkSimple extSym)
-  let ty ← liftMacroM <| withRef type <| expandIrResultTypeToC false decl.resultType
-  let params ← liftMacroM <| mkParams info.type bvs decl.params
-  let body := packBody body
-  let fn ← MonadRef.withRef Syntax.missing <| `(function|
-    extern "C" LEAN_EXPORT%$ex $ty:cTypeSpec $id:ident($params:params) $body:compStmt
-  )
-  let cmd ← `(alloy c section $fn:function end)
-  withMacroExpansion (← getRef) cmd <| elabCommand cmd
-
-
-scoped syntax (name := leanCppExport) "extern \"C\"" : cDeclSpec
-
-scoped elab (name := externCppDecl) doc?:(docComment)?
-"alloy " &"cpp " ex:&"extern " sym?:(str)? attrs?:(Term.attributes)?
-"def " id:declId bx:binders " : " type:term " := " body:cStmt : command => do
-
-  -- Lean Definition
-  let name := (← getCurrNamespace) ++ id.raw[0].getId
-  let (symLit, extSym) :=
-    match sym? with
-    | some sym => (sym, sym.getString)
-    | none =>
-      let extSym := "_alloy_c_" ++ name.mangle
-      (Syntax.mkStrLit extSym <| SourceInfo.fromRef id, extSym)
-  let attr ← withRef ex `(Term.attrInstance| extern $symLit:str)
-  let attrs := #[attr] ++ expandAttrs attrs?
-  let bs := bx.raw.getArgs.map (⟨.⟩)
-  let cmd ← `($[$doc?]? @[$attrs,*] opaque $id $[$bs]* : $type)
-  withMacroExpansion (← getRef) cmd <| elabCommand cmd
-  let bvs ← liftMacroM <| bs.concatMapM matchBinder
-  let id := mkIdentFrom symLit (Name.mkSimple extSym)
-  let ty ← liftMacroM <| withRef type <| expandIrResultTypeToC false decl.resultType
-  let params ← liftMacroM <| mkParams info.type bvs decl.params
-  let body := packBody body
-  let fn ← MonadRef.withRef Syntax.missing <| `(function|
-    extern "C" LEAN_EXPORT%$ex $ty:cTypeSpec $id:ident($params:params) $body:compStmt
-  )
-  let cmd ← `(alloy c section $fn:function end)
-  withMacroExpansion (← getRef) cmd <| elabCommand cmd
   elabExternImpl exTk sym? ⟨id.raw[0]⟩ bvs ty (packBody stmts)
